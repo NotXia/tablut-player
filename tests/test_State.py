f530d136e19de071e230e2f838902b57632757d0
NotXia <35894453+NotXia@users.noreply.github.com>
2023-11-14 16:34:15 +0100

    Update test

--- tests/test_State.py
+++ tests/test_State.py
@@ -1,6 +1,6 @@
 import os, sys
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../src"))
-from State import *
+from gametree.State import *
 import numpy as np
 import unittest
 
